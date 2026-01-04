88adfaca041567a52c0921c41b08bd062819fd5a
lirilabs <lirilabs@gmail.com>
2026-01-04 11:19:59 +0530

    Update trending.js

--- api/trending.js
+++ api/trending.js
@@ -1,8 +1,9 @@
 export function getTrending(contents) {
   return Object.values(contents)
-    .sort((a,b) =>
-      (b.likes * 2 + (Date.now()-b.createdAt)/1e6) -
-      (a.likes * 2 + (Date.now()-a.createdAt)/1e6)
-    )
+    .sort((a, b) => {
+      const scoreA = a.likes * 2 + (Date.now() - a.createdAt) / 1e6;
+      const scoreB = b.likes * 2 + (Date.now() - b.createdAt) / 1e6;
+      return scoreB - scoreA;
+    })
     .slice(0, 20);
 }
