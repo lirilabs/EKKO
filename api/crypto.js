d396f91f8fc7ca4535a7216fc45fbd72384fcf58
lirilabs <lirilabs@gmail.com>
2026-01-04 11:47:56 +0530

    Update crypto.js

--- api/crypto.js
+++ api/crypto.js
@@ -8,8 +8,10 @@ export async function getCrypto(keyHex) {
     encrypt(obj) {
       const iv = crypto.randomBytes(12);
       const cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
+
       let enc = cipher.update(JSON.stringify(obj), "utf8", "base64");
       enc += cipher.final("base64");
+
       return {
         iv: iv.toString("hex"),
         data: enc,
@@ -20,14 +22,18 @@ export async function getCrypto(keyHex) {
     decrypt(payload) {
       try {
         if (!payload?.iv || !payload?.data || !payload?.tag) return null;
+
         const decipher = crypto.createDecipheriv(
           "aes-256-gcm",
           key,
           Buffer.from(payload.iv, "hex")
         );
+
         decipher.setAuthTag(Buffer.from(payload.tag, "hex"));
+
         let dec = decipher.update(payload.data, "base64", "utf8");
         dec += decipher.final("utf8");
+
         return JSON.parse(dec);
       } catch {
         return null;
