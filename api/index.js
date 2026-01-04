a56db5cffd8900cac6e5523ee29810f1835efc38
lirilabs <lirilabs@gmail.com>
2026-01-04 11:22:01 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,17 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Content-Type", "application/json");
 
+  const { action } = req.query;
+
+  if (action === "ping") {
+    return res.status(200).json({
+      ok: true,
+      pong: Date.now()
+    });
+  }
+
   return res.status(200).json({
-    ok: true,
-    message: "EKKO API is alive"
+    ok: false,
+    message: "Unknown action"
   });
 }
